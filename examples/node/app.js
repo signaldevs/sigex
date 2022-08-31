bf82886417278a94f9a9aa40abd8edcfff0482ae
Kevin O'Hara <kevinohara80@gmail.com>
2022-08-31 12:14:42 -0400

    chore: cleaned up example and added cli var

--- examples/node/app.js
+++ examples/node/app.js
@@ -1,4 +1,5 @@
 
 console.log(`DEV_VAR: ${process.env.DEV_VAR}`);
 console.log(`OVERRIDE_TEST: ${process.env.OVERRIDE_TEST}`);
+console.log(`CLI_VAR: ${process.env.CLI_VAR}`);
 console.log(`GCP_SECRET: ${process.env.GCP_SECRET}`);
